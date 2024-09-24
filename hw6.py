dbbab0b57f77cf7333f850dbab615ad5ad4dca0d
Tacha <Anastasiykoshe@gmail.com>
2024-09-24 10:11:35 +0300

    модуль 9 задание 6

---
+++ hw6.py
@@ -0,0 +1,16 @@
+def all_variants(text):
+    length = len(text)
+    result = []
+    stack = [('', 0)]
+    while stack:
+        current, index = stack.pop()
+        if current:
+            result.append(current)
+        for s in range(index, length):
+            stack.append((current + text[s], s + 1))
+    return result
+
+
+a = all_variants('abc')
+for i in a:
+    print(i)
